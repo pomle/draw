f1b34ba7318227d98cbcfb1f1c8fe1917ced1795
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-01 19:13:19 +0100

    Add clear controls.

--- src/Play/Draw/index.jsx
+++ src/Play/Draw/index.jsx
@@ -8,6 +8,14 @@ import surface from './draw.svg';
 import './Draw.css';
 
 class Draw extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDrawing: false,
+    };
+  }
+
   componentDidMount() {
     const context = this.canvas.getContext('2d');
     context.lineWidth = 3;
@@ -15,6 +23,9 @@ class Draw extends Component {
     this.draw = createDrawer(context);
 
     this.surface.addEventListener('load', () => {
+      this.surface.contentDocument.addEventListener('touchstart', this.touch);
+      this.surface.contentDocument.addEventListener('touchend', this.release);
+
       this.sensor = createSensor(this.surface);
 
       this.sensor.listen(data => {
@@ -30,14 +41,44 @@ class Draw extends Component {
   }
 
   componentWillUnmount() {
+      this.surface.contentDocument.removeEventListener('touchstart', this.touch);
+      this.surface.contentDocument.removeEventListener('touchend', this.release);
       this.sensor.destroy();
+  }
+
+  touch = () => {
+    console.log('touch');
+    this.setState({
+      isDrawing: true,
+    });
+  }
+
+  release = () => {
+    this.setState({
+      isDrawing: false,
+    });
+  }
+
+  clear = () => {
+    this.props.conn.send({
+      type: 'clear',
+    });
   }
 
   render() {
     const {word} = this.props;
+    const {isDrawing} = this.state;
+
+    const hide =  {
+      display: isDrawing ? 'none' : null,
+    };
 
     return (
       <div className="Draw">
+        <div className="controls" style={hide}>
+          <button>Clear</button>
+        </div>
+
         <Fill aspect={800/450}>
           <object
             data={surface}
@@ -47,7 +88,7 @@ class Draw extends Component {
 
           <canvas width="800" height="450" ref={node => this.canvas = node}/>
 
-          <h2 className="subjectCaption">Draw: {word}</h2>
+          <h2 className="subjectCaption" style={hide}>Draw: {word}</h2>
         </Fill>
       </div>
     );
